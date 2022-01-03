32dac85a508d1189d92bc2e39125b01a26e57a11
Grisha Angelov <grisha.angelov89@gmail.com>
2022-01-03 19:18:32 +0200

    Add test cases in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,76 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 
-test('renders learn react link', () => {
+import App from './App'
+
+test('should render Linear Search Algorithm', () => {
+  render(<App />)
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Linear Search Algorithm$/)
+})
+
+test('should render Binary Search Algorithm', () => {
+  render(<App />)
+
+  act(() => {
+    const binarySearchLink = document.querySelector("a[href='#/binary-search']")
+    binarySearchLink.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Binary Search Algorithm$/)
+})
+
+test('should render Bubble Sort Algorithm', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  act(() => {
+    const bubbleSortLink = document.querySelector("a[href='#/bubble-sort']")
+    bubbleSortLink.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Bubble Sort$/)
+})
+
+test('should render Quick Sort Algorithm', () => {
+  render(<App />)
+
+  act(() => {
+    const bubbleSortLink = document.querySelector("a[href='#/quick-sort']")
+    bubbleSortLink.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Quick Sort$/)
+})
+
+test.skip('should render Binary Tree Algorithms', () => {
+  render(<App />);
+
+  act(() => {
+    const binaryTreeLink = document.querySelector("a[href='#/binary-tree']")
+    binaryTreeLink.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Binary Tree Traversal Algorithms$/)
+})
+
+test('should render Caesar Cipher Algorithm', () => {
+  render(<App />)
+
+  act(() => {
+    const bubbleSortLink = document.querySelector("a[href='#/caesar-cipher']")
+    bubbleSortLink.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Caesar Cipher Algorithm$/)
+})
+
+test('should render Flood Fill Algorithm', () => {
+  render(<App />)
+
+  act(() => {
+    const bubbleSortLink = document.querySelector("a[href='#/flood-fill']")
+    bubbleSortLink.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(screen.getAllByRole('heading')[1]).toHaveTextContent(/^Flood Fill Algorithm$/)
+})
