3c0b5faf9dc111a4c200ca21f99a8603f1ede1da
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-19 09:34:53 +0200

    Change the extension for components to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+
+import Layout from './components/Layout'
+import Sidebar from './components/Sidebar'
+
+import LinearSearch from './components/algorithms/LinearSearch'
+import BinarySearch from './components/algorithms/BinarySearch'
+import BubbleSort from './components/algorithms/BubbleSort'
+import QuickSort from './components/algorithms/QuickSort'
+import BinaryTree from './components/algorithms/BinaryTree'
+import CaesarCipher from './components/algorithms/CaesarCipher'
+import FloodFill from './components/algorithms/FloodFill'
+
+const layoutWith = Component => (
+  <Layout sidebar={<Sidebar />}>
+    <Component />
+  </Layout>
+)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Navigate replace to='/linear-search' />} />
+        <Route path='/linear-search' element={layoutWith(LinearSearch)} />
+        <Route path='/binary-search' element={layoutWith(BinarySearch)} />
+        <Route path='/bubble-sort' element={layoutWith(BubbleSort)} />
+        <Route path='/quick-sort' element={layoutWith(QuickSort)} />
+        <Route path='/binary-tree' element={layoutWith(BinaryTree)} />
+        <Route path='/caesar-cipher' element={layoutWith(CaesarCipher)} />
+        <Route path='/flood-fill' element={layoutWith(FloodFill)} />
+      </Routes>
+    </Router>
   )
 }
 
